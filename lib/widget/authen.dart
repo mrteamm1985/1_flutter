777d1f5802cb9cc141b6b78163fec9214c770802
mrteamm1985 <teamm_mac@Teamms-MacBook-Pro.local>
2020-09-21 14:35:02 +0700

    error red eye

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -6,14 +7,59 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+
+bool statusRedeye =true;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(width: 180,
-          child: Image.asset('images/logo.png')
+  
+                  child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+            buildContainer(), 
+            buildText(), 
+            buildContainerTextF(),
+            buildContainerTextFPass(),
+              ],
           ),
-      ),
+        ),
     );
+  }
+  
+
+  Container buildContainerTextF() =>  Container(margin: EdgeInsets.only(top:26),
+    width: 250,child: TextField(
+    decoration: InputDecoration(labelText: 'User Name',
+    border: OutlineInputBorder()),
+    )
+    );
+
+   Container buildContainerTextFPass() =>  Container(margin: EdgeInsets.only(top:26),
+    width: 250,child: TextField(obscureText: true,
+    decoration: InputDecoration(labelText: 'Pass Word',
+    border: OutlineInputBorder(),
+    suffixIcon: IconButton(icon: icon.re, onPressed: )
+    ),
+    //suff
+    )
+    );
+
+  Text buildText() => Text(
+        'PWA My Team',
+        style: GoogleFonts.rubik(
+            textStyle: TextStyle(
+          //fontWeigh:FontWeight.bold,
+          fontSize: 30,
+          color: Colors.pink.shade700,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold,
+        )),
+      );
+
+  Container buildContainer() {
+    return Container(width: 180, child: Image.asset('images/logo.png'));
   }
 }
