d8cdcb8f13b102f05ec3700f4d8f876ded96a971
mrteamm1985 <teamm_mac@Teamms-MacBook-Pro.local>
2020-09-22 11:08:31 +0700

    map Div

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -6,7 +6,26 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  get border => null;
+  //get border => null;
+
+  List<String> positions = [
+    'Users',
+    'SA',
+    'Front End',
+    'Back End',
+    'Full Stack'
+  ];
+  String choosePosition;
+  double lat,lng;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -15,27 +34,107 @@ class _RegisterState extends State<Register> {
           backgroundColor: Colors.purple.shade700,
           title: Text('Register'),
         ),
-        body: Column(children: [
-          buildContainerAvatar(),
-          buildContainerName(),
-        ]));
+        body: SingleChildScrollView(
+          child: Column(children: [
+            buildContainerAvatar(),
+            buildContainerName(),
+            buildSizedBox(),
+            buildContainerDDL(),
+            buildSizedBox(),
+            buildContainerUser(),
+            buildSizedBox(),
+            buildContainerPass(),
+            buildSizedBox(),
+            buildContainerMAP(),
+          ]),
+        ));
   }
+
+  Container buildContainerMAP() => Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.width * 0.8,
+        color: Colors.grey,
+      );
+
+  SizedBox buildSizedBox() {
+    return SizedBox(
+      height: 16,
+    );
+  }
+
+  Container buildContainerDDL() => Container(
+        width: 250,
+        child: DropdownButton<String>(
+          items: positions
+              .map((e) => DropdownMenuItem(
+                    child: Text(e),
+                    value: e,
+                  ))
+              .toList(),
+          value: choosePosition,
+          hint: Text('Position'),
+          onChanged: (value) {
+            setState(() {
+              choosePosition = value;
+            });
+          },
+        ),
+      );
 
   Container buildContainerName() {
     return Container(
         width: 250,
         child: TextField(
-          decoration: InputDecoration(prefixIcon: Icon(Icons.face,color:Color(0xffe81ee8)),
-              enabledBorder:OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: BorderSide(color: Colors.red),
-                ),
-
-              focusedBorder:OutlineInputBorder(
+          decoration: InputDecoration(
+            hintText: 'Display Ur Name',
+            prefixIcon: Icon(Icons.face, color: Color(0xffe81ee8)),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(30),
               borderSide: BorderSide(color: Colors.blue),
-                ),
-                  ),
+            ),
+          ),
+        ));
+  }
+
+  Container buildContainerUser() {
+    return Container(
+        width: 250,
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'User Name',
+            prefixIcon: Icon(Icons.account_box, color: Color(0xffe81ee8)),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+          ),
+        ));
+  }
+
+  Container buildContainerPass() {
+    return Container(
+        width: 250,
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Ur Password',
+            prefixIcon: Icon(Icons.keyboard, color: Color(0xffe81ee8)),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+          ),
         ));
   }
 
